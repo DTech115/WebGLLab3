beb23150d94ae23142cae939ef31bee2a0447714
House <41124462+DTech115@users.noreply.github.com>
2026-09-15 01:30:35 +0000

    tested translate camera

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WebGLLab3Part1
+
+## Step 1. Is there any change in the display? Why or why not?
+No, the axis lines remain as the same transformation is produced by both.
--- boxes.js
+++ boxes.js
@@ -189,7 +189,8 @@ function render() {
 	var at =  vec3(0.0, 0.0, 0.0);
 	var up =  vec3(0.0, 1.0, 0.0);
 
-	modelViewMatrix = lookAt(eye,at,up);
+	// modelViewMatrix = lookAt(eye,at,up);
+	modelViewMatrix = translate(0, 0, -10);
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
 	
 	gl.drawArrays(shapes.axes.type, shapes.axes.start, shapes.axes.size);	
